d10df2f57c81ec83768c185ec620225dac7e36c4
siddharth <notcodesid@gmail.com>
2024-12-29 09:25:06 -0500

    add shacd cn

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,3 +3,19 @@ export const theme = {
   extend: {},
 };
 export const plugins = [];
+
+export const references = [
+  {
+    path: "./tsconfig.app.json"
+  },
+  {
+    path: "./tsconfig.node.json"
+  }
+];
+
+export const compilerOptions = {
+  baseUrl: ".",
+  paths: {
+    "@/*": ["./src/*"]
+  }
+};
